8084b50089558c707ac413600fd4652ac09f470a
Stanislas Marçais / Knupel <stanislasiii@gmail.com>
2025-01-20 18:45:48 +0100

    boucle de logo

--- src/components/grid/grid_logo.tsx
+++ src/components/grid/grid_logo.tsx
@@ -9,7 +9,16 @@ import React, { FC } from "react";
 import { useState } from "react";
 // GATSBY
 import { useStaticQuery, graphql } from "gatsby";
-import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import { GatsbyImage } from "gatsby-plugin-image";
+// APP
+import { get_css_value } from "../../utils/hu.tsx";
+
+
+const style_image = {
+  // maxWidth: "300px",
+  border: "2px solid " +get_css_value("--color_border"),
+  borderRadius: "10px",
+}
 
 
 function build_list(edges : any, list : any) {
@@ -25,7 +34,6 @@ function build_list(edges : any, list : any) {
         is_over: false,
         text: ""
       }
-      // elem_index++;
       for({node} of edges) {
         if(node.extension === "md" && obj.img.name === node.name) {
           obj.text = node.childrenMarkdownRemark[0];
@@ -38,19 +46,20 @@ function build_list(edges : any, list : any) {
   return list;
 }
 
-
-const img_grid_style = {
-  position: "relative",
-  display: "grid",
-  gridTemplateColumns: `repeat(auto-fill, minmax(200px, 1fr))`,
-};
-
-
-interface Props {
-  // data? : any;
+interface PropsVignette {
+  elem : any;
+  index : number;
 }
 
-export const GridLogo: FC<Props> = () => {
+const Vignette: FC<PropsVignette> = (elem, index) => {
+  // je ne comprends pas pourquoi je dois écrire elem.elem pur rentrer dans l'élément
+  return elem !== null? <div style={style_image}><GatsbyImage image={elem.elem.node.childImageSharp.gatsbyImageData} alt={String(index)}/></div> : <></>;
+}
+
+
+interface PropsGrid {}
+
+export const GridLogo: FC<PropsGrid> = () => {
   const { allFile } = useStaticQuery(
     graphql`
       query {
@@ -71,27 +80,16 @@ export const GridLogo: FC<Props> = () => {
       }
     `
   );
+
+
   const [list, set_list] = useState([]);;
   if(list.length === 0) {
     set_list(build_list(allFile.edges, list));
   }
-  console.log("liste client", list);
-  return <>
-    {list.map((elem : any, index : number) => (
-              elem !== null ? <GatsbyImage image={elem.img} alt={String(index)}/> : null
-            ))}
-         
-  </>
+  return <div className="grid grid-cols-2 md:grid-cols-3 gap-3 p-4 max-w-[400px] md:max-w-[600px] place-items-center">
+    { allFile.edges.map((elem : any, index : number) => {
+        return <Vignette elem={elem} index={index}/>
+      }
+    )}       
+  </div>
 }
-
-
-
-  // if(list !== null) {
-  //   return (
-  //     <GridImage style={img_grid_style} list={list}/>
-  //   );
-  // } else return null;
-
-
-
-  // <GatsbyImage image={getImage(elem.img)} alt={elem.img.name}/>
